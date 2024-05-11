5f432f2d6ff3920025abd8d778c61bf52ae6bc72
Tulika <tulika.sharma@innovasolutions.com>
2024-05-11 17:44:21 +0530

    Way one of connecting to DB, making the connection in the index file using an IIFE

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import { DB_NAME } from "./constants";
 import express from "express";
 const app = express()
 
-;(async () => { //; is placed before an IIFE as a common practice to avoid certain issues
-    try {
-        await mongoose.conect(`${process.env.MONGOB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log('ERROR: ', error)
-            throw error
-        })
+// ;(async () => { //; is placed before an IIFE as a common practice to avoid certain issues
+//     try {
+//         await mongoose.conect(`${process.env.MONGOB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log('ERROR: ', error)
+//             throw error
+//         })
 
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port: ${process.env.PORT}`)
-        })
-    }catch (error){
-        console.error("ERROR: ", error)
-        throw error
-    }
-})()
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port: ${process.env.PORT}`)
+//         })
+//     }catch (error){
+//         console.error("ERROR: ", error)
+//         throw error
+//     }
+// })()
