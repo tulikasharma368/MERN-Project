f2e04cf4c84ee676b51377f53f4663c0c76a9916
Tulika <tulika.sharma@innovasolutions.com>
2024-05-11 19:38:19 +0530

    Way 2 of connecting DB | loading env variables in the beginning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: '/env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
 
-import express from "express";
-const app = express()
+dotenv.config({
+    path: './env'
+})
 
+connectDB()
+
+// import express from "express";
+// const app = express()
 // ;(async () => { //; is placed before an IIFE as a common practice to avoid certain issues
 //     try {
 //         await mongoose.conect(`${process.env.MONGOB_URI}/${DB_NAME}`)
